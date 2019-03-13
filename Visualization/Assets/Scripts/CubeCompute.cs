4a5a7aa11d8d7e70b5b6d41a98a15e50bbba1b5c
unknown <youndg03@gmail.com>
2019-03-13 19:31:57 +0900

    sphere

---
+++ Visualization/Assets/Scripts/CubeCompute.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCompute : MonoBehaviour
+{
+	struct Particle
+	{
+		public Vector3 position;
+		public Vector4 color;
+	}
+
+	public Material material;
+	public ComputeShader computeShader;
+	
+	private int particleCount = 1000000;
+	private int mComputeShaderKernelID;
+	private ComputeBuffer particleBuffer;
+	private int mWarpCount;
+
+	private const int SIZE_PARTICLE = 28;
+	private const int WARP_SIZE = 256;
+
+	private void Start()
+	{
+		InitComputeShader();
+	}
+	void InitComputeShader()
+	{
+		mWarpCount = Mathf.CeilToInt((float)particleCount / WARP_SIZE);
+
+		Particle[] particleArray = new Particle[particleCount];
+		int i = 0;
+		for (int a = -50; a < 50; a++)
+		{
+			for (int b= -50; b < 50; b++)
+			{
+				for (int c = -50; c < 50; c++)
+				{
+					float x = a;
+					float y = b;
+					float z = c;
+
+					Vector3 xyz = new Vector3(x, y, z);
+					xyz.Normalize();
+					xyz *= Random.value * 5f; // 산개!
+					
+					particleArray[i].position = xyz;
+					particleArray[i].color = new Vector4((i+50)%100, (i + 50) % 100, (i + 50) % 100, 1);
+					i++;
+				}
+			}
+		}
+
+		particleBuffer = new ComputeBuffer(particleCount, SIZE_PARTICLE);
+		particleBuffer.SetData(particleArray);
+
+		mComputeShaderKernelID = computeShader.FindKernel("CSCube");
+
+		computeShader.SetBuffer(mComputeShaderKernelID, "particleBuffer", particleBuffer);
+		material.SetBuffer("particleBuffer", particleBuffer);
+	}
+
+	private void OnRenderObject()
+	{
+		material.SetPass(0);
+		Graphics.DrawProcedural(MeshTopology.Points, 1, particleCount);
+	}
+}
---
+++ Visualization/Assets/Shader/CubeShader.shader
@@ -0,0 +1,54 @@
+﻿Shader "Custom/Cube" {
+
+	SubShader{
+		Pass{
+		Tags{ "RenderType" = "Opaque" }
+		LOD 200
+		Blend SrcAlpha one
+
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+#pragma vertex vert
+#pragma fragment frag
+
+#include "UnityCG.cginc"
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+#pragma target 5.0
+
+		struct Particle {
+		float3 position;
+		float4 color;
+	};
+
+	struct PS_INPUT {
+		float4 position : SV_POSITION;
+		float4 color : COLOR;
+	};
+	// particles' data
+	StructuredBuffer<Particle> particleBuffer;
+
+
+	PS_INPUT vert(uint vertex_id : SV_VertexID, uint instance_id : SV_InstanceID)
+	{
+		PS_INPUT o = (PS_INPUT)0;
+
+		o.color = fixed4(particleBuffer[instance_id].position, 1.0f);
+
+		// Position
+		o.position = UnityObjectToClipPos(float4(particleBuffer[instance_id].position, 1.0f));
+
+		return o;
+	}
+
+	float4 frag(PS_INPUT i) : COLOR
+	{
+		return i.color;
+	}
+
+
+		ENDCG
+	}
+	}
+		FallBack Off
+}
