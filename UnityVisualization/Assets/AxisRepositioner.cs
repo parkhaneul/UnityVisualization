34a61e38c4db48f96d3e19760c6e02453989d292
parkhaneul <c6h6cl6@ajou.ac.kr>
2019-04-29 14:27:27 +0900

    axis_repositioner
    2019_04_29

--- UnityVisualization/Assets/AxisRepositioner.cs
+++ UnityVisualization/Assets/AxisRepositioner.cs
@@ -5,21 +5,54 @@ using UnityEngine;
 public class AxisRepositioner : MonoBehaviour
 {
     public Camera view;
-    public GameObject plane;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    public float value = 1;
+    public LineRenderer myRenderer;
+    public Collider sphereCollider;
+    private bool isClicked = false;
 
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
         Ray ray = view.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit) && Input.GetMouseButton(0))
+        int layerMask = 1 << 9;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask) && hit.transform.tag == "axis")
         {
-            plane.transform.LookAt(view.transform);
-            this.transform.position = new Vector3(hit.point.x,hit.point.y,0);
+            if (Input.GetMouseButtonDown(0))
+            {
+                isClicked = true;
+            }
+        }
+
+        if (isClicked && Physics.Raycast(ray, out hit) && hit.transform.tag == "axis_sphere")
+        {
+            Vector3 temp1, temp2;
+            temp1 = hit.point;
+            if(Physics.Raycast(temp1,view.transform.forward, out hit))
+            {
+                temp2 = hit.point;
+                if (Vector3.Distance(transform.position, temp1) > Vector3.Distance(transform.position, temp2))
+                {
+                    transform.position = temp2;
+                }
+                else
+                {
+                    transform.position = temp1;
+                }
+            }
+            else
+            {
+                transform.position = temp1;
+            }
+        }
+
+        if (isClicked)
+        {
+            myRenderer.SetPosition(1, transform.position);
+            if (Input.GetMouseButton(0))
+            {
+                isClicked = false;
+            }
         }
     }
 }
