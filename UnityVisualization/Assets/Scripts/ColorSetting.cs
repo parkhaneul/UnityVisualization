a74c98c2c6b6917c4358db058ece118f5449c4e5
parkhaneul <c6h6cl6@ajou.ac.kr>
2019-04-30 13:39:10 +0900

    update
    2019_04_30_1

--- UnityVisualization/Assets/Scripts/ColorSetting.cs
+++ UnityVisualization/Assets/Scripts/ColorSetting.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class vectorSetting : MonoBehaviour
+public class ColorSetting : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
