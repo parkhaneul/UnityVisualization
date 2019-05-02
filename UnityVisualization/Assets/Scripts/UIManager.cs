86b58541563222f11ebf22528fb88b6e36c8cb24
unknown <youndg03@gmail.com>
2019-05-02 15:32:01 +0900

    viewlist

---
+++ UnityVisualization/Assets/Scripts/UIManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+	public GameObject Content;
+	public GameObject PropertySelector;
+    public void OnClickAddPropertySelector()
+	{
+		GameObject g = GameObject.Instantiate(PropertySelector);
+		g.transform.parent = Content.transform;
+		g.transform.localScale = Vector3.one;
+		Debug.Log("propertyselector추가 기능을 구현해야 합니다.");
+		//propertyselector추가
+	}
+}
