366df5d45d40b2916e305ecc4635ff8a341ff5f4
parkhaneul <c6h6cl6@ajou.ac.kr>
2019-05-07 15:18:16 +0900

    commit

---
+++ UnityVisualization/Assets/Scripts/AxisSetting.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisSetting : MonoBehaviour
+{
+	//public GameObject vectorController;
+
+	public PropertyListContent propertyListContent;
+    public GameObject propertyController;
+
+	private List<propertySetting> propertySettings = new List<propertySetting>();
+
+    private Axis currentAxis;
+	public static int index;
+	
+
+	public void SetAxis(int _index)
+    {
+        index = _index;
+        currentAxis = SetDropDown.myAxis[index];
+		
+        ChangePropertiesInfo();
+    }
+
+    private void ChangePropertiesInfo()
+    {
+		foreach(Transform p in propertyListContent.transform)
+		{
+			GameObject.Destroy(p.gameObject);
+		}
+		propertySettings.Clear();
+		foreach (Weight w in currentAxis.weights)
+		{
+			Debug.Log(w.propertyIndex + " :: " + w.weight);
+			ActiveProperty(w);
+		}
+	}
+
+	public void OnClickAddPropertySelector()
+	{
+		Weight w = new Weight();
+		w.propertyIndex = 0;
+		w.weight = 0;
+		SetDropDown.myAxis[index].weights.Add(w);
+
+		ActiveProperty(w);
+	}
+
+	private void ActiveProperty(Weight w)
+	{
+		GameObject g = GameObject.Instantiate(propertyController);
+		g.transform.parent = propertyListContent.transform;
+		g.transform.localScale = Vector3.one;
+		g.GetComponent<propertySetting>().dropdown.value = w.propertyIndex;
+		g.GetComponent<propertySetting>().slider.value = w.weight;
+		g.GetComponent<propertySetting>().text.text = w.weight.ToString();
+		g.GetComponent<propertySetting>().SettingIndex = propertySettings.Count;
+		propertySettings.Add(g.GetComponent<propertySetting>());
+	}
+}
---
+++ UnityVisualization/Assets/Scripts/propertySetting.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class propertySetting : MonoBehaviour
+{
+    public Dropdown dropdown;
+    public Slider slider;
+    public Text text;
+	public int SettingIndex;
+
+	//private int propertyIndex;
+    //public Weight currentWeight;
+
+    public void Start()
+    {
+        for(int i = 0; i < MetaData.floatArray.Length; i++)
+        {
+            var temp = new Dropdown.OptionData(Header.data[MetaData.floatArray[i]]);
+            dropdown.options.Add(temp);
+        }
+    }
+
+    public void changeValue()
+    {
+        text.text = slider.value.ToString();
+
+		Weight temp = SetDropDown.myAxis[AxisSetting.index].weights[SettingIndex];
+		temp.propertyIndex = dropdown.value;
+		temp.weight = slider.value;
+		SetDropDown.myAxis[AxisSetting.index].weights[SettingIndex] = temp;
+	}
+	/*
+    public Weight GetWeight()
+    {
+        var temp = new Weight();
+        temp.propertyIndex = dropdown.value;
+        temp.weight = slider.value;
+        return temp;
+    }
+	*/
+}
